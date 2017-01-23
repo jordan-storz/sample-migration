ac203a413de9a08cf43cb5d0a61ef3b87222239c
jordan-storz <jordan.storz.1@gmail.com>
2017-01-23 10:28:32 -0700

    seeds create all markers

--- marker-seed.sql
+++ marker-seed.sql
@@ -1,7 +1,8 @@
 \c marker_tracker;
+truncate marker;
 alter sequence marker_id_seq restart with 3;
 
 insert into marker (id, color, quality, toxic, user_id) values (1, 'red', 'Decent', FALSE, 1);
 insert into marker (id, color, quality, toxic, user_id) values (2, 'blue', 'Great', FALSE, 1);
 insert into marker (id, color, quality, toxic, user_id) values (3, 'green', 'Decent', FALSE, 1);
-insert into marker (id, color, quality, toxic, user_id) values (2, 'brown', 'Does not work', TRUE, 2);
+insert into marker (id, color, quality, toxic, user_id) values (4, 'brown', 'Does not work', TRUE, 2);
--- user-seed.sql
+++ user-seed.sql
@@ -1,5 +1,5 @@
 \c marker_tracker;
+truncate "user";
 alter sequence user_id_seq restart with 3;
--- truncate 'user' restart identity cascade;
 insert into "user" (id, email, password) values (1, 'jstorz@example.com', '123Pass');
 insert into "user" (id, email, password) values (2, 'email@example.com', 'password55');
